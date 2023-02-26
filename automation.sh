9738a3b5d36e84c59f5cb32c8056c8bddf60096f
root <root@ip-172-31-53-12.ec2.internal>
2023-02-26 10:13:48 +0000

    Updated autmoation.sh by enhance functionallity added cronjob check and inventory.html check

--- automation.sh
+++ automation.sh
@@ -39,6 +39,23 @@ fi
 #check create a tar file for error and access logs of apache in /var/log/apache2 dir
 
 tar -cf /tmp/${myname}-httpd-logs-${timestamp}.tar /var/log/apache2/*log
+tar_size=$(du -sh /tmp/${myname}-httpd-logs-${timestamp}.tar |awk '{print $1}')
 
 #check upload tar to aws s3 bucket for backup
 aws s3 cp /tmp/${myname}-httpd-logs-${timestamp}.tar s3://${s3_bucket}/${myname}-httpd-logs-${timestamp}.tar
+
+#part 3
+#Check whether inventory.html is present of not in directory /var/www/html
+if [ -f "/var/www/html/inventory.html" ]; then
+	echo "inventory.html is already present"
+        echo "httpd-logs	$timestamp	tar	$tar_size" >>/var/www/html/inventory.html
+else
+	echo "Log Type	Time Created	Type	Size" >/var/www/html/inventory.html
+fi
+
+#check cronjob is created or not and create if not
+if [ -f "/etc/cron.d/automation" ]; then
+	echo "Cron file automation is already present"
+else
+	echo "30 3 * * * root /root/Automation_Project/automation.sh | tee /tmp/output_script.log" >/etc/cron.d/automation
+fi
